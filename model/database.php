797a4f7d7023a8210966e5623b50583027a4cbee
dan-stachlewski <dan.stachlewski@gmail.com>
2015-10-09 13:40:34 +1100

    Created the Database Class - responsible for connecting to the dBase This example uses private static properties and public static methods - illustrates how we don't need a __constructor() or create an Object to connect to the dBase. Using PHP PDO to Connect, Create, Read, Update, Delete Postcode records (CRUD),

---
+++ model/database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database {
+    //SET the dBase connection vars
+    private static $dsn = 'mysql:host=localhost;dbname=postcodes';
+    private static $username = 'root';
+    private static $password = 'password';
+    private static $db;
+
+    private function __construct() {
+
+    }//END private function __construct
+
+    /**
+     * Create a dBase connection by using a try catch stmnt
+     * @return type $db connection
+     */
+    public static function getDB() {
+        if (!isset(self::$db)) {
+            try {
+                self::$db = new PDO(self::$dsn, self::$username, self::$password);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                exit();
+            }
+        }
+        echo "Database connected Successfully!";
+        return self::$db;
+    }//END public static function getDB
+
+}//END class Database
